70ffbf175f38ead631fd8ff969361939add9e771
Lesha Shirokov <shirokov44007@gmail.com>
2021-06-02 21:34:25 +0300

    test commit

---
+++ StorageService/PostNew.swift
@@ -0,0 +1,17 @@
+//
+//  PostNew.swift
+//  StorageService
+//
+//  Created by Apple on 31.05.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+public struct PostNew {
+    public let author: String
+    public let description: String
+    public let image: UIImage
+    public let likes: String
+    public let viewsCount: String
+}
